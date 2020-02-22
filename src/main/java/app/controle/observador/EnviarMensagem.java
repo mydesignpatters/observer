c6271188bb29c081390aa380bcd0deeac642ec42
armandossrecife <armando.sousa@gmail.com>
2020-02-22 09:55:21 -0300

    Foi criado uma classe EnviarMensagem para monitorar as alterações e enviar mensagens a cada modificação das ações da carteira de ações.

---
+++ src/main/java/app/controle/observador/EnviarMensagem.java
@@ -0,0 +1,27 @@
+package app.controle.observador;
+
+import java.util.Date;
+
+public class EnviarMensagem implements Observador{
+    String mensagem="";
+
+    @Override
+    public void mudancaQuantidade(String acao, Integer qtd) {
+        mensagem = "A ação " + acao + " teve uma variação de " + qtd;
+        enviar(mensagem);
+    }
+    
+    public void enviar(String mensagem){
+        System.out.println("Mensagem " + mensagem + " enviada as " + this.dataCorrente());
+    }
+
+    public String dataCorrente(){
+        String data;
+        Date minhaData = new Date();
+
+        data = "" + minhaData.getHours() + ":" + minhaData.getMinutes() + ":" + minhaData.getSeconds() + " - " + 
+        minhaData.getDay() + "/" +  minhaData.getMonth() + "/" +minhaData.getYear();
+        
+        return data;
+    }
+}
--- src/main/java/app/controle/principal/App.java
+++ src/main/java/app/controle/principal/App.java
@@ -1,20 +1,46 @@
 package app.controle.principal;
 
 import app.controle.observador.AcoesLogger;
+import app.controle.observador.EnviarMensagem;
 import app.controle.observador.GraficoBarras;
 import app.controle.negocio.CarteiraAcoes;
 
 public class App {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         System.out.println("Teste do padrao Observer");
 
         GraficoBarras graficoBarras = new GraficoBarras();
         AcoesLogger acoesLogger = new AcoesLogger();
         CarteiraAcoes carteiraAcoes = new CarteiraAcoes();
-        
+        EnviarMensagem enviarMensagem = new EnviarMensagem();
+
         carteiraAcoes.addObservador(graficoBarras);
         carteiraAcoes.addObservador(acoesLogger);
+        carteiraAcoes.addObservador(enviarMensagem);
 
+        Thread.sleep(2000);
+        carteiraAcoes.adicionaAcoes("COMP02", 200);
+
+        Thread.sleep(2000);
+        carteiraAcoes.adicionaAcoes("EMP34", 400);
+
+        Thread.sleep(2000);
+        carteiraAcoes.adicionaAcoes("ACDF89", 300);
+        
+        Thread.sleep(2000);
+        carteiraAcoes.adicionaAcoes("EMP34", -200);
+
+        Thread.sleep(2000);
+        carteiraAcoes.adicionaAcoes("COMP02", 150);
+
+        Thread.sleep(2000);
+        carteiraAcoes.adicionaAcoes("COMP02", 200);
+
+        Thread.sleep(2000);
+        carteiraAcoes.adicionaAcoes("EMP34", 400);
+
+        Thread.sleep(2000);
+        carteiraAcoes.adicionaAcoes("ACDF89", 300);
         Thread.sleep(2000);
         carteiraAcoes.adicionaAcoes("COMP02", 200);
 
